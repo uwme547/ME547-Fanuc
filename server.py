eaa7b3975e56aac70d38499112b6424fee2a2a28
Cem Cetin <cembcetin@gmail.com>
2020-03-09 15:51:47 -0400

    first commit

--- server.py
+++ server.py
@@ -4,5 +4,8 @@ if __name__ == "__main__":
     robot = Fanuc() 
     robot.setSpeed(20)
     robot.moveToPosition(400, -100, -100, 0)
+    robot.setSpeed(50)
+    robot.moveJoints(10,0,0,0,0)
+    robot.moveLin(435,-100,-120,-180,0,90)
     time.sleep(5)
   
